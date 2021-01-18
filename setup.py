3768b4cb9cc7e0769accaa17a4ae4d8022cdfbb9
Neetx <neetx@protonmail.com>
2021-01-18 23:33:58 +0100

    Installation package

---
+++ setup.py
@@ -0,0 +1,37 @@
+"""
+Copyright 2021 Neetx
+This file is part of ropgadgetlib.
+ropgadgetlib is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+ropgadgetlib is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with ropgadgetlib.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ropgadgetlib",
+    version="0.0.1",
+    author="Antonio Pastorelli",
+    author_email="neetx@protonmail.com",
+    description="rop gadget extractor library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Neetx/ropgadgetlib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License:: GPL-3.0 License",
+        "Operating System :: OS Linux",
+    ],
+    python_requires='>=3.7',
+)
